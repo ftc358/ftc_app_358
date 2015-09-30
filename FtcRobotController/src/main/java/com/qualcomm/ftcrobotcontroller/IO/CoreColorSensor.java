bd07d9f46e0cf2d9c4b7030af042c0a51133e503
ftc358 <jliu@webb.org>
2015-09-30 16:50:27 -0700

    Created dummy IO classes

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/IO/CoreColorSensor.java
@@ -0,0 +1,8 @@
+package com.qualcomm.ftcrobotcontroller.IO;
+
+/**
+ * Created by jamesliu on 9/30/15.
+ */
+public class CoreColorSensor extends I2CPeripheral {
+
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/IO/I2CPeripheral.java
@@ -0,0 +1,16 @@
+package com.qualcomm.ftcrobotcontroller.IO;
+
+/**
+ * Created by jamesliu on 9/30/15.
+ */
+public class I2CPeripheral {
+
+    public I2CPeripheral() {
+
+    }
+
+    public int init(){
+
+        return 0;
+    }
+}
